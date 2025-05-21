c41bf53bc3fa686c68c90f3ac1354dac30904537
munirix <munira.osmanmourad@gmail.com>
2025-05-21 09:33:02 -0300

    Leitor de Arquivos: Testar

--- src/LeitorArquivos.java
+++ src/LeitorArquivos.java
@@ -8,15 +8,44 @@ import java.util.List;
  */
 public class LeitorArquivos {
     public static int[] readDataset(String filename) throws IOException {
+        // Extrai o tipo (ex: "sorted" de "sorted_10000.txt")
+        String tipo = filename.split("_")[0]; // Pega tudo antes do primeiro "_"
+        
+        // Monta o caminho completo: "tipo/nomeDoArquivo"
+        String caminhoCompleto = tipo + File.separator + filename;
+        
+        // Lê o arquivo normalmente
         List<Integer> list = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(caminhoCompleto))) {
             String line;
             while ((line = br.readLine()) != null) {
                 list.add(Integer.parseInt(line));
             }
         }
+        
+        // Converte List<Integer> para int[]
         int[] arr = new int[list.size()];
-        for (int i = 0; i < arr.length; i++) arr[i] = list.get(i);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = list.get(i);
+        }
         return arr;
     }
 }
+
+
+
+ 
+// public class LeitorArquivos {
+//     public static int[] readDataset(String filename) throws IOException {
+//         List<Integer> list = new ArrayList<>();
+//         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+//             String line;
+//             while ((line = br.readLine()) != null) {
+//                 list.add(Integer.parseInt(line));
+//             }
+//         }
+//         int[] arr = new int[list.size()];
+//         for (int i = 0; i < arr.length; i++) arr[i] = list.get(i);
+//         return arr;
+//     }
+// }
